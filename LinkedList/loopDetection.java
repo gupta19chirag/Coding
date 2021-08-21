64182cff2133cb836c19b17f4c6d88886936711c
gupta19chirag <63705416+gupta19chirag@users.noreply.github.com>
2021-08-21 14:01:42 +0530

    loopDetectionUsingLinkedList.java
    1st approach:- Using hashing, hashMap will be used in which address of each node is stored as a key and in its value the frequency of that node.
    2nd approach:- Explained above

---
+++ LinkedList/loopDetection.java
@@ -0,0 +1,11 @@
+class loopDetection{
+  static boolean detect(Node head){
+    Node fast = head, slow = head;
+    while(fast != null && fast.next!=null){
+      fast = fast.next.next;
+      slow = slow.next;
+      if(fast == slow) return true;      
+    }
+    return false;
+  }
+}
