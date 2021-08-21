65d79d484e2155393b2e305d8a4655fb5b856cf3
gupta19chirag <63705416+gupta19chirag@users.noreply.github.com>
2021-08-21 14:23:12 +0530

    Create startingNodeOfCycle.java

---
+++ LinkedList/startingNodeOfCycle.java
@@ -0,0 +1,18 @@
+class startingNodeOfCycle{
+  public ListNode detectCycle(ListNode head){
+    ListNode fast = head, slow = head;
+    while(fast!=null && fast.next!=null){
+      fast = fast.next.next;
+      slow = slow.next;
+      if(fast == slow){
+        slow = head;
+        while(slow != fast){
+          slow = slow.next;
+          fast = fast.next;
+        }
+        return slow;
+      }
+    }
+    return null; // if not present
+  }
+}
