80e8a89a43dc9a10e821ac70de7638efa24f67e0
gupta19chirag <63705416+gupta19chirag@users.noreply.github.com>
2021-08-21 11:58:07 +0530

    Create partitionList.java

---
+++ LinkedList/partitionList.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode first = new ListNode(0);
+        ListNode f = first;
+      
+        ListNode second = new ListNode(0);
+        ListNode s = second;
+        
+        while(head != null){
+            if(head.val < x){
+                f.next = head;
+                f = f.next;
+            }
+            else{
+                s.next = head;
+                s = s.next;
+            }
+            head = head.next;
+        }
+        s.next = null;
+        f.next = second.next;
+    return first.next;
+    }    
+}
