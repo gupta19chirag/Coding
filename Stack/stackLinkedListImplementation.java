d99419e003efdf4f3479a1e9cb52c0e85be034bf
gupta19chirag <63705416+gupta19chirag@users.noreply.github.com>
2021-08-23 09:20:13 +0530

    Create stackLinkedListImplementation.java

---
+++ Stack/stackLinkedListImplementation.java
@@ -0,0 +1,20 @@
+class stackLinkedListImplementation{
+  //public static int MAX = 1000;
+  int top;
+  
+  stackLinkedListImplementation(){
+    top = -1;
+  }
+  
+public static void main(String[] args){
+  stackLinkedListImplementation stack = new stackLinkedListImplementation();
+  
+  }
+}
+class Node{
+  int val;
+  Node temp;
+  Node(int key){
+    val = key;
+  }
+}
