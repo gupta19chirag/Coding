70bd4c1c49da53269b97222291bc8af613b308ff
gupta19chirag <63705416+gupta19chirag@users.noreply.github.com>
2021-08-23 09:50:03 +0530

    Update stackLinkedListImplementation.java

--- Stack/stackLinkedListImplementation.java
+++ Stack/stackLinkedListImplementation.java
@@ -1,14 +1,41 @@
 class stackLinkedListImplementation{
   //public static int MAX = 1000;
-  int top;
-  
+  static Node root;
+  int top;  
   stackLinkedListImplementation(){
-    top = -1;
+    root = null;
   }
-  
+  boolean isEmpty(){
+   if(root == null){
+    return true;
+   }
+    return false;
+  }  
+  void push(int key){
+    Node temp = new Node(key);
+    temp.next = root;
+    root = temp;
+  }
+  int pop(){
+    int del = Integer.MIN_VALUE;
+    if(isEmpty()){      
+    }
+    else{
+      del = root.val;
+      root = root.next;
+    }
+    return del;
+  }
+  int peek(){
+    int topEle = Integer.MAX_VALUE;
+    if(isEmpty()) 
+    else topEle = root.val;
+    return topEle;
+  }
 public static void main(String[] args){
   stackLinkedListImplementation stack = new stackLinkedListImplementation();
-  
+    stack.push(10);
+    System.out.println(stack.pop());
   }
 }
 class Node{
@@ -16,5 +43,6 @@ class Node{
   Node temp;
   Node(int key){
     val = key;
+    temp = null;
   }
 }
